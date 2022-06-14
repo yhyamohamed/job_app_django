4f237c4dc53595fbf82ba8b4442139ec5d33994f
yhyamohamed <yhyashehata1@gmail.com>
2022-06-14 20:11:30 +0200

    construct admin page

--- job_App/accounts/models.py
+++ job_App/accounts/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class User(AbstractUser):
     Types = (('developer', 'developer'), ('recruiter', 'recruiter'))
