23826b0181e2fc9b09664950129ed97614e6e179
Mahmoud Mohamed Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2022-06-14 20:54:28 +0200

    fixing admin-py models-py

--- job_App/accounts/admin.py
+++ job_App/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'address', 'user_type', 'is_active', 'date_joined', 'all_tags']
+    search_fields = ['username', 'tags']
+
+    def get_fields(self, request, obj=None):
+        fields = super(UserAdmin, self).get_fields(request, obj)
+        print(fields)
+        if obj:
+            fields_to_remove = []
+            if request.user.is_superuser:
+                fields_to_remove = ["is_staff",
+                                    "is_superuser",
+                                    "groups",
+                                    "user_permissions","date_of_birth",'allow_mail_notification' ,'date_joined',]
+            for field in fields_to_remove:
+                fields.remove(field)
+        return fields
+
+    def all_tags(self, obj):
+        return [tag.name for tag in obj.tags.all()]
+# admin.site.register(User)
