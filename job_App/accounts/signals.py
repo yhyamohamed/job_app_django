79099e0766f5ffe441ac8e724970b37641403127
yhyamohamed <yhyashehata1@gmail.com>
2022-06-15 23:32:59 +0200

    admin mail notification

---
+++ job_App/accounts/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
+from .models import User
+
+
+@receiver(post_save, sender=User)
+def my_handler(sender, **kwargs):
+    user = kwargs.get('instance')
+    if user.user_type == 'recruiter':
+        send_mail('new recruiter has beenn added ')
+        send_mail(
+            'new recruiter has been added ',  # subject
+            user.email + ' has been created ',  # message
+            'system@admin.com',  # from
+            'admin@admin.com',  # to
+        )
